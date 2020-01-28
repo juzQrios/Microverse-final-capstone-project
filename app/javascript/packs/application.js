1c85d1169198653df6b6f7752a1dad015c9154e7
juzQrios <dj30c.1@gmail.com>
2020-01-29 02:41:40 +0530

    chore: fix linter errors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,4 +18,5 @@ require('channels');
 // Support component names relative to this directory:
 const componentRequireContext = require.context('components', true);
 const ReactRailsUJS = require('react_ujs');
+
 ReactRailsUJS.useContext(componentRequireContext);

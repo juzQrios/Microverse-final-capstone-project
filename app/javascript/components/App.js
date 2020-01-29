350ea97a852889c21ea6346af47bfd1c6690c880
juzQrios <dj30c.1@gmail.com>
2020-01-30 02:40:58 +0530

    Fix lint errors

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 
 const App = () => (
-  <Container maxWidth='sm' style={{ textAlign: 'center' }}>
+  <Container maxWidth="sm" style={{ textAlign: 'center' }}>
     <h1>Work in Progress</h1>
   </Container>
 );

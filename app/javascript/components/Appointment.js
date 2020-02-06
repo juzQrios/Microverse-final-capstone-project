33797c5f6c7604c13d9d85b745f707821bf407fb
juzQrios <dj30c.1@gmail.com>
2020-02-07 01:24:16 +0530

    Style Appointments Page

--- app/javascript/components/Appointment.js
+++ app/javascript/components/Appointment.js
@@ -1,16 +1,39 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { format, parseISO } from 'date-fns';
+import ListHeader from './styled/ListHeader';
+
+const Card = styled.div`
+  margin: 0.5em;
+  padding: 0.6em;
+  box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.20);
+  border-radius: 2px;
+`;
+
+const CardDetails = styled.div`
+  margin: 0.5em 0;
+  ${'span'} {
+    font-weight: bold;
+  }
+`;
 
 const Appointment = ({ appointment, doctor }) => (
-  <div className="appointment">
-    <div>{`Appointment ID : ${appointment.id}`}</div>
-    <div>{`Doctor: ${doctor.name}`}</div>
-    <div>{`Date: ${format(parseISO(appointment.date), 'dd/MM/yyyy')}`}</div>
-    <div>{`Time: ${format(parseISO(appointment.time), 'hh:mm:ss aa')}`}</div>
-    <hr />
-  </div>
+  <Card className="appointment">
+    <ListHeader
+      title={doctor.name}
+      subTitle={`Appointment ID: ${appointment.id}`}
+    />
+    <CardDetails>
+      <span>Date : </span>
+      {format(parseISO(appointment.date), 'dd/MM/yyyy')}
+    </CardDetails>
+    <CardDetails>
+      <span>Time : </span>
+      {format(parseISO(appointment.time), 'hh:mm:ss aa')}
+    </CardDetails>
+  </Card>
 );
 
 Appointment.propTypes = {

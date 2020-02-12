7c6f3fdedc3383c20d9b9db7ba8f9ddb96216e16
juzQrios <dj30c.1@gmail.com>
2020-02-13 02:02:32 +0530

    Improve LinkButton component's design

--- app/javascript/components/styled/LinkButton.js
+++ app/javascript/components/styled/LinkButton.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import RoundButton from './RoundButton';
 
 const LinkButton = styled(RoundButton)`
-  padding: 7px 21px;
+  padding: 10px 21px;
   margin: 0.5em 0;
   width: auto;
   text-align: center;
